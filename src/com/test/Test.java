780655d7ec914f0b261cfe54aba1e4e215404577
yaugdin <yaugdin@126.com>
2015-05-18 15:31:50 +0800

    不能全部提交

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -2,7 +2,11 @@ package com.test;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+
+
+
 
 import javax.servlet.ServletContext;
 
@@ -21,6 +25,39 @@ public class Test {
 		File f = new File("D:/hekai/2b6568e3-8628-40a4-a9f9-34ec941eee00.jpg");
 		f.renameTo(new File("D:/hekai/2b6568e3.jpg"));
 	    
+		Map mapList= new HashMap();
+		
+		ClassDetail c= new ClassDetail();
+		c.setClassId(1);
+		c.setUserName("1");
+		c.setId(1);
+		
+		ClassDetail c1= new ClassDetail();
+		c1.setClassId(2);
+		c1.setUserName("2");
+		c1.setId(2);
+		
+		
+		ClassDetail c2= new ClassDetail();
+		c2.setClassId(3);
+		c2.setUserName("3");
+		c2.setId(3);
+		
+		ClassDetail c3= new ClassDetail();
+		c3.setClassId(4);
+		c3.setUserName("4");
+		c3.setId(4);
+		
+		mapList.put("1", c);
+		mapList.put("2", c1);
+		mapList.put("3", c2);
+		mapList.put("4", c3);
+		
+		Iterator  it=mapList.entrySet().iterator();
+		while(it.hasNext()){
+			System.out.print(it.next() + " ");  
+		}
+		
 		
 
 	}
